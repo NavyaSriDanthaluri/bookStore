c91412276ce8c3bc56d40b0ba109849b6703182d
NavyaSriDanthaluri <114234265+NavyaSriDanthaluri@users.noreply.github.com>
2022-11-15 10:06:14 +0530

    Add files via upload

---
+++ BookStore-Book/src/main/java/com/book/store/util/BookDTOConverter.java
@@ -0,0 +1,24 @@
+package com.book.store.util;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.book.store.dto.DefaultResponseDTO;
+import com.book.store.entity.Book;
+
+@Component
+@Scope("singleton")
+public class BookDTOConverter {
+	
+	public static DefaultResponseDTO  getDefaultDTO(Book book)
+	{
+		DefaultResponseDTO dto = new DefaultResponseDTO(
+				book.getTitle(),
+				book.getPrice(),
+				book.getAuthorName(),
+				book.getCategory());
+		
+		return dto;
+	}
+
+}
